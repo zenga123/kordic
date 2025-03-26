67549aa83981c38f5c6442f3dbe71dac1b99140e
zenga123 <zenga85@naver.com>
2025-03-26 22:05:33 +0900

    Initial Commit

---
+++ kordic/kordicApp.swift
@@ -0,0 +1,17 @@
+//
+//  kordicApp.swift
+//  kordic
+//
+//  Created by musung on 2025/03/26.
+//
+
+import SwiftUI
+
+@main
+struct kordicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
